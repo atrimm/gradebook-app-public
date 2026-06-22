763fc0af2eaac4118886e500cf6e75bd3d7f8b6b
Anderson Trimm <trimm.anderson@gmail.com>
2026-06-22 14:55:33 -0500

    update progress check policy

--- student_portal.py
+++ student_portal.py
@@ -230,7 +230,9 @@ st.dataframe(
 )
 
 st.subheader("Progress Check Eligibility")
+st.markdown("Progress checks are given approximately one week after the initial check is returned.")
 st.markdown("To be eligible for a progress check, all corresponding classwork and homework assignments must be complete before the initial check, corrections of the intial check must be made, and all assigned additional practice completed.")
+st.markdown("If you are ineligible for a progress check on a given standard, then your next attempt at that standard will be on the final exam.")
 
 if eligibility_issues.empty:
     st.info("You are eligible for all upcoming progress checks.")
