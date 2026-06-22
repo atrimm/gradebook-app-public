ddc897cda0208aece420042d1922b32ae3792fc1
Anderson Trimm <trimm.anderson@gmail.com>
2026-06-22 14:33:41 -0500

    progress check eligibility description added

--- student_portal.py
+++ student_portal.py
@@ -231,9 +231,10 @@ st.dataframe(
 )
 
 st.subheader("Progress Check Eligibility")
+st.markdown("To be eligible for a progress check, all corresponding classwork and homework assignments must be complete before the initial check, corrections of the intial check must be made, and additional practice completed."
 
 if eligibility_issues.empty:
-    st.info("No progress check eligibility issues.")
+    st.info("You are eligible for all upcoming progress checks.")
 else:
     st.dataframe(
         eligibility_issues,
