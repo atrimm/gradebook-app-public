4d462fe0d7c986f599edb0b88fad0b3f20a4ed82
Anderson Trimm <trimm.anderson@gmail.com>
2026-06-23 00:18:06 -0500

    update messaging in student portal

--- student_portal.py
+++ student_portal.py
@@ -177,9 +177,7 @@ absence_history = dashboard_data["absence_history"]
 general_comments = dashboard_data["general_comments"]
 
 st.info(
-    "PowerSchool is the official grade record, but is not very supportive of standards-based grading. "
-    "This portal provides more detailed information about progress towards meeting the course standards, "
-    "score history, comments, progress check eligibility, and your current semester grade."
+    "PowerSchool is the official grade record. Because PowerSchool is designed primarily for traditional weighted average-based grading, this portal provides more detailed information about progress toward meeting the course standards, score history, comments, progress check eligibility, and the current semester grade."
 )
 
 st.subheader("Semester Grade")
